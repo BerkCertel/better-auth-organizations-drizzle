03c0724fe481843c12bf1f5934c763eb33ddceae
Berk Certel <m.berkcertel@outlook.com>
2025-09-18 01:12:40 +0300

    login page created and sign and signup func added

---
+++ server/users.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+
+export const signIn = async (email: string, password: string) => {
+  try {
+    await auth.api.signInEmail({
+      body: {
+        email,
+        password,
+      },
+    });
+
+    return { success: true, message: "Signed in successfully." };
+  } catch (error) {
+    const e = error as Error;
+    return {
+      success: false,
+      message: { error: e.message || "Unknown error occurred." },
+    };
+  }
+};
+
+export const signUp = async () => {
+  await auth.api.signUpEmail({
+    body: {
+      email: "berkdev@test.com",
+      password: "password123",
+      name: "Berk",
+    },
+  });
+};
